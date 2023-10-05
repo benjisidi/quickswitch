7a92416056aeb45658acfabee080149eaad5e469
Benjamin Sidi <benji@incident.io>
2023-10-06 00:00:06 +0100

    Nicer rendering

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,19 @@
 #!/usr/bin/env -S npx ts-node
 // see https://github.com/cookpete/git-checkout-interactive/blob/master/index.js
 
-import { simpleGit } from "simple-git";
 import { search } from "fast-fuzzy";
 import prompts from "prompts";
 import util from "util";
 import { exec } from "child_process";
 import chalk from "chalk";
 const execPromise = util.promisify(exec);
+
+const truncate = (msg: string, max = 50) => {
+  if (msg.length > max) {
+    return msg.slice(0, max - 3).trim() + "...";
+  }
+  return msg;
+};
 
 type Branch = {
   name: string;
@@ -19,11 +25,10 @@ type Branch = {
 const displayBranch = (branch: Branch) => {
   return `${chalk.yellow(branch.name)}\t${chalk.green(
     branch.lastCommit
-  )}\t${chalk.blue(branch.message)}\t${chalk.magenta(branch.author)}`;
+  )}\t${chalk.blue(truncate(branch.message))}\t${chalk.magenta(branch.author)}`;
 };
 
 const main = async () => {
-  const git = simpleGit();
   const rawBranches = await execPromise(
     "git for-each-ref --sort=-committerdate refs/heads --format='%(refname:short)|%(HEAD)%(refname:short)|%(committerdate:relative)|%(subject)|%(authorname)' --color=always"
   );
@@ -73,7 +78,7 @@ const main = async () => {
   if (target.name.startsWith("*")) {
     return;
   }
-  await git.checkout(target.name.trim());
+  await execPromise(`git checkout ${target.name.trim()}`);
 };
 
 main();
